1ada9290f04ffbae4bd9ec9065729fc69940e888
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 17:57:10 -0600

    agregando src/test/java/pages/tc_02Page.java

---
+++ src/test/java/pages/tc_02Page.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class tc_02Page {
+    WebDriver driver;
+
+    By usernameField = By.id("username");
+    By passwordField = By.id("password");
+    By loginButton = By.id("loginButton");
+    By errorMessage = By.id("errorMessage");
+
+    public tc_02Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        driver.findElement(loginButton).click();
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        return driver.findElement(errorMessage).isDisplayed();
+    }
+}
